c86e072a97384849883a81cc90cff5467233e543
debayudh2001 <debayudh31@gmail.com>
2026-02-18 13:22:36 +0530

    Removed strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
-import { StrictMode } from 'react'
+//import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './app/store'
 import './index.css'
 import App from './App.jsx'
+import Practice from './practice.jsx'
+import PracticeContextProvider from './practiceContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </StrictMode>,
 )
+
+
